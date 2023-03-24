09451b98b91cbad5ceec7acfdc0e95f32e778288
Lesya <jamma@i.ua>
2023-03-24 22:44:11 +0100

    Add basic markup

---
+++ src/apiServise.ts
@@ -0,0 +1,7 @@
+// const API_KEY = "odBDoQ8iA96IyYTnxrPkKyfuhh1LWvbC";
+// const BASE_URL = "https://api.apilayer.com/fixer";
+// // const BASE_URL = "https://api.apilayer.com/fixer/convert?to={to}&from={from}&amount={amount}";
+
+// export const getCurrency = () => {
+//   return fetch(`${BASE_URL}/convert`, { headers: { apikey: API_KEY } });
+// };
---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+// import { getCurrency } from "../apiServise";
+
+const Header = () => {
+  // useEffect(() => {
+  //   getCurrency()
+  //     .then(res => res.json())
+  //     .then(data => console.log(data));
+  // }, []);
+  return (
+    <header>
+      <div>
+        <p></p>
+        <p></p>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
